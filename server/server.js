8a7ac80ce7318bc9c190ee087e755c7e5c5e665c
AMIT TIWARY <amittiwary710@gmail.com>
2018-06-20 03:09:56 +0530

    changed PORT

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port=process.env.MONGODB_URI||3000;
+const port=process.env.PORT||3000;
 
 app.use(bodyParser.json());
 
